80d7180c2153e63a5e5016212abca1578dab0385
GPMcKelvey <gpmckelvey@gmail.com>
2021-03-11 14:23:00 -0500

    dustin is breaking everything

--- server/controllers/usercontroller.js
+++ server/controllers/usercontroller.js
@@ -1,29 +1,9 @@
 const Express = require('express');
-
-
-router.get('/test', (req, res) => {
-    res.send('Hey! This is a user test route!')
-});
-
-module.exports = router;const Express = require('express');
 const router = Express.Router();
 
 router.get('/test', (req, res) => {
     res.send('Hey! This is a user test route!')
 });
 
-module.exports = router;const Express = require('express');
-const router = Express.Router();
-
-router.get('/test', (req, res) => {
-    res.send('Hey! This is a user test route!')
-});
-
-module.exports = router;const Express = require('express');
-const router = Express.Router();
-
-router.get('/test', (req, res) => {
-    res.send('Hey! This is a user test route!')
-});
 
 module.exports = router;
