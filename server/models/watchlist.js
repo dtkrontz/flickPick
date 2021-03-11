600e74987cb077735ebcab33ccdad02e3395bf2a
Dtkrontz <dtkrontz@gmail.com>
2021-03-11 17:07:26 -0500

    Watchlist Controller and Model constructed

---
+++ server/models/watchlist.js
@@ -0,0 +1,41 @@
+// stretch - ratings 
+
+const {DataTypes} = require('sequelize');
+const db = require('../db');
+
+const Watchlist = db.define('watchlist', {
+    title: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    rated: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    runtime: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    genre: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    plot: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    poster: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    watched: {
+        type: DataTypes.BOOLEAN,
+        allowNull: true
+    },
+    recommend: {
+        type: DataTypes.BOOLEAN,
+        allowNull: true
+    }
+})
+
+module.exports = Watchlist;
