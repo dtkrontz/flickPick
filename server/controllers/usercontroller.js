c209885436d7faec5fddc1b4d1dee2baeabf7416
Dtkrontz <dtkrontz@gmail.com>
2021-03-11 14:13:16 -0500

    test2

--- server/controllers/usercontroller.js
+++ server/controllers/usercontroller.js
@@ -5,4 +5,25 @@ router.get('/test', (req, res) => {
     res.send('Hey! This is a user test route!')
 });
 
+module.exports = router;const Express = require('express');
+const router = Express.Router();
+
+router.get('/test', (req, res) => {
+    res.send('Hey! This is a user test route!')
+});
+
+module.exports = router;const Express = require('express');
+const router = Express.Router();
+
+router.get('/test', (req, res) => {
+    res.send('Hey! This is a user test route!')
+});
+
+module.exports = router;const Express = require('express');
+const router = Express.Router();
+
+router.get('/test', (req, res) => {
+    res.send('Hey! This is a user test route!')
+});
+
 module.exports = router;
