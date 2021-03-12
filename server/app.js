652251ae165744a0ddd68663730cced6e0c77e4a
GPMcKelvey <gpmckelvey@gmail.com>
2021-03-12 09:42:48 -0500

    second 3/12

--- server/app.js
+++ server/app.js
@@ -1,11 +1,26 @@
+require("dotenv").config();
+
 const Express = require('express');
+const dbConnection = require('./db');
+const middleware = require('./middleware');
 const app = Express();
+
+app.use(middleware.CORS);
+app.use(Express.json());
 
 const controllers = require("./controllers");
 
 app.use("/watchlist", controllers.watchlistController);
 app.use("/user", controllers.userController);
 
-app.listen(3000, () => {
-    console.log(`[Server]: App is listening on 3000.`);
-})
+dbConnection.authenticate()
+    .then(() => dbConnection.sync())
+    .then(() => {
+        app.listen(process.env.PORT, () => {
+    console.log(`[Server]: App is listening on ${process.env.PORT}`);
+});
+    })
+    .catch((err) => {
+        console.log(`[Server]: Server crashed. Error = ${err}`);
+    });
+
