a09733384f779dcc8d30f0586fcfcc703cfe7547
GPMcKelvey <gpmckelvey@gmail.com>
2021-03-12 17:21:53 -0500

    user update

--- server/controllers/usercontroller.js
+++ server/controllers/usercontroller.js
@@ -4,24 +4,22 @@ const {UserModel}= require('../models');//calling back to the userModel
 const bcrypt = require('bcryptjs');
 const jwt = require ('jsonwebtoken');
 const { UniqueConstraintError } = require('sequelize/lib/errors');
-
-
 //register
 router.post('/register', async (req, res) => {
-    let {username, password}= req.body;
+    let {username, password}= req.body.user;
     try{
-        const newUser = await UserModel.create({
+        const User = await UserModel.create({
             username,
             password: bcrypt.hashSync(password,13)
         })
         const token = jwt.sign({
-            id:newUser.id
+            id:User.id
         },
         process.env.JWT_SECRET,
         {expiresIn : 60*60*24});
         res.status(201).json({
             Message:'User was registered!',
-            user: newUser,
+            user: User,
             token
         })
     } catch (error) {
@@ -30,28 +28,25 @@ router.post('/register', async (req, res) => {
                 message:'Do you have a doppelganger? This username is already in use!'
             })
         }else{
+            console.log(error)
             res.status(500).json({
                 error: "Failed to register user"
             })
         }
     }
 });
-
 //login
 router.post('/login', async(req, res) => {
-    const {username, password} = req.body;
+    const {username, password} = req.body.user;
     try{
        let loginUser = await UserModel.findOne({
            where:{
                username
            }
        });
-
        if(loginUser) {
            let passwordComparison = await bcrypt.compare(password,loginUser.password);
-
            if(passwordComparison){
-
                let token = jwt.sign(
                    {
                        id: loginUser.id
@@ -61,7 +56,6 @@ router.post('/login', async(req, res) => {
                        expiresIn:60*60*24
                    }
                );
-
                res.status(200).json({
                    user: loginUser,
                    message:"User successfully logged in!",
@@ -77,13 +71,10 @@ router.post('/login', async(req, res) => {
                message:"Incorrect email or password"
            })
        }
-
     }catch(err){
         res.status(500).json({
             message:"Error logging in!"
         });
-
     }
 })
-
 module.exports = router;
